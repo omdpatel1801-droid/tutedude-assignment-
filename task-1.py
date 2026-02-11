71f663d0b5cd14c6e03e67972effb4befc575d7d
omdpatel1801-droid <omdpatel1801@gmail.com>
2026-02-11 17:09:28 +0530

    tutedude assignment 11-02-26

---
+++ task-1.py
@@ -0,0 +1,7 @@
+#arthimetic op
+a=int(input("enter the number a "))
+b=int(input("enter the number b "))
+print(f"addition {a+b}")
+print(f"subtraction {a-b}")
+print(f"multiplication {a*b}")
+print(f"division {a/b}")
---
+++ task-2.py
@@ -0,0 +1,3 @@
+fname=input("enter the first name is ")
+lname=input("enter the last name is ")
+print(f"hello, {fname} {lname}! welcome to python program")
